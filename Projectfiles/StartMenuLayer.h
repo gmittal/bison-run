154e6e76ffe0b301f0df7c111eb40b24b464568a
Gautam Mittal <gautam@mittal.net>
2013-08-28 18:22:33 -0700

    Update

---
+++ Projectfiles/StartMenuLayer.h
@@ -0,0 +1,13 @@
+//
+//  StartMenuLayer.h
+//  Bison Run
+//
+//  Created by Gautam Mitttal on 7/25/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "CCScene.h"
+
+@interface StartMenuLayer : CCScene
+
+@end
